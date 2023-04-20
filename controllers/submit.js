e3cfc059ae89f24657132be2ac80d40132208d7d
secretivo <secretivo@secretivos-MacBook-Air.local>
2023-04-20 00:21:24 -0400

    mongoDB connect

--- controllers/submit.js
+++ controllers/submit.js
@@ -18,6 +18,53 @@ router.post('/signup', (req, res) => {
     res.send(`Thank you for signing up, ${username}!`); 
 });
 
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://angelicaerazo22:qXUybw0gcKDQQTTa@sei.3zeldhq.mongodb.net/test';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function createSubmission(submission) {
+  try {
+    await client.connect();
+    const database = client.db('menstrual_app');
+    const submissions = database.collection('submissions');
+    await submissions.insertOne(submission);
+    console.log('Submission saved to database');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = (req, res) => {
+  const submission = {
+    name: req.body.name,
+    email: req.body.email,
+    symptoms: req.body.symptoms,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    intensity: req.body.intensity,
+    painLocation: req.body.painLocation,
+  };
+  createSubmission(submission);
+  res.render('submit', { title: 'Submitted!' });
+};
+
+
+module.exports = (req, res) => {
+  const submission = {
+    name: req.body.name,
+    email: req.body.email,
+    symptoms: req.body.symptoms,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    intensity: req.body.intensity,
+    painLocation: req.body.painLocation,
+  };
+  createSubmission(submission);
+  res.render('submit', { title: 'Submitted!' });
+};
+
 module.exports = router;
 
 
