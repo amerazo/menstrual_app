855bcf2636452bdaef1a642b40c3312ed8667c2d
secretivo <secretivo@secretivos-Air.lan>
2023-04-22 16:58:27 -0400

    route

--- server.js
+++ server.js
@@ -1,230 +1,203 @@
-// Dependencies
+//Dependencies
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
-require('dotenv').config();
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const secretKey = process.env.SECRET_KEY;
-
-
-//mongoDB
-const store = new MongoDBStore({
-  uri: 'mongodb+srv://menstrual:i0r3D6rayayhy5bo@sei.3zeldhq.mongodb.net/superperiodapp',
-  collection: 'mySessions'
-});
-
-store.on('error', function(error) {
-  console.log(error);
-});
-
-//// Set SESSION_SECRET environment variable
-process.env.SESSION_SECRET = 'mysecretvalue';
-
-//middleware
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Session middleware
-app.use(session({
-  name: 'sessionId',
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: store,
-  cookie: { secure: false }
-}));
-
-app.use(morgan('dev'));
-
-
-
-//Mongoose
-process.env.MONGODB_URI = 'mongodb+srv://menstrual:i0r3D6rayayhy5bo@sei.3zeldhq.mongodb.net/superperiodapp';
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB Atlas');
-}).catch((error) => {
-  console.log(error);
+
+// Set up app
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+//reference https://expressjs.com/en/starter/static-files.html
+//https://www.geeksforgeeks.org/express-js-express-static-function/
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+// Set up database connection
+//https://stackoverflow.com/questions/68234879/usenewurlparser-true-useunifiedtopology-true-what-is-the-use-of-this-in-mon
+mongoose.connect('mongodb+srv://menstrual:i0r3D6rayayhy5bo@sei.3zeldhq.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+ console.log('Database connected');
 });
 
 
-//Period Schema
+//reference: https://youtu.be/9OfL9H6AmhQ
 const periodSchema = new mongoose.Schema({
-  startDate: Date,
-  endDate: Date,
-  flowLength: Number,
-  notes: String,
-  flowLevel: String,
-  cramps: String,
-  emotional: String,
-  physical: String,
-  otherPhysical: String,
-  discharge: String,
-  temperature: Number,
+ startDate: Date,
+ endDate: Date,
+ flowLength: Number,
+ notes: String,
+ flowLevel: String,
+ cramps: String,
+ emotional: [String],
+ //attempting to create it into an array because it has multiple options
+ physical: [String],
+ otherPhysical: String,
+ discharge: String,
+ temperature: Number,
 });
 
-// Create data model
+
+
+
+
+
+// Create data model for period
 const Period = mongoose.model('Period', periodSchema);
 
-// Home Route
+
+// Define home route
 app.get('/', (req, res) => {
-    res.render("home.ejs");
-})
+ res.render('home');
+});
 
-// About Route
-app.get('/views/about.ejs', (req, res) => {
-  res.render("about.ejs");
-})
 
-//Period Routes
+//-----------CRUD-------// CRUD ------------------------- // CRUD -----------------------------//
+//-----------CRUD-------// CRUD ------------------------- // CRUD -----------------------------//
+//-----------CRUD-------// CRUD ------------------------- // CRUD -----------------------------//
+
+
 app.get('/periods', async (req, res) => {
-  const periods = await Period.find({});
-  res.render('index', { periods });
+ //create the periods const to make it easier to navigate
+ const periods = await Period.find({});
+ res.render('index', { periods });
 });
 
-//Period Add
+
+//calling periods and new.ejs file
 app.get('/periods/new', (req, res) => {
-  res.render('new');
+ res.render('new');
 });
 
+
+//calling periods and allows to POST new entries
 app.post('/periods', async (req, res) => {
-  const period = new Period(req.body.period);
-  await period.save();
-  res.redirect('/periods');
+//https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+ const period = new Period(req.body.period);
+ await period.save();
+ res.redirect('/periods');
 });
 
-//Period Show
+
 app.get('/periods/:id', async (req, res) => {
-  const period = await Period.findById(req.params.id);
-  res.render('show', { period });
+ const period = await Period.findById(req.params.id);
+ //https://stackoverflow.com/questions/58251870/findbyidreq-params-id-returns-null-as-response
+ res.render('show', { period });
 });
 
-//Period Edit
+
+//calling periods and to edit the files. find the id and edit it.
 app.get('/periods/:id/edit', async (req, res) => {
-  const period = await Period.findById(req.params.id);
-  res.render('edit', { period });
+ const period = await Period.findById(req.params.id);
+ res.render('edit', { period });
 });
+
 
 app.put('/periods/:id', async (req, res) => {
-  const { id } = req.params;
-  const period = await Period.findByIdAndUpdate(id, { ...req.body.period });
-  res.redirect(`/periods/${period.id}`);
+ const { id } = req.params;
+//req.body allows to access data in a string or JSON object from the client side. Generally use the req. body object to receive data through POST and PUT requests in the Express server.
+ const period = await Period.findByIdAndUpdate(id, { ...req.body.period, emotional: req.body.period.emotional || [], physical: req.body.period.physical || [] }, { new: true });
+ res.redirect(`/periods/${period.id}`);
 });
 
-//Period Delete
+
+//Used Besides College Reference for findByID and Update and FindbyID and delete and findbyID
+//https://youtu.be/cwa6LciFPmA
+//Delete entries
 app.delete('/periods/:id', async (req, res) => {
-  const { id } = req.params;
-  await Period.findByIdAndDelete(id);
-  res.redirect('/periods');
+ const { id } = req.params;
+ await Period.findByIdAndDelete(id);
+ res.redirect('/periods');
 });
 
-// Sign Up Form
-app.get('/signup', async (req, res) => {
-  const periods = await Period.find({});
-  res.render("signupindex.ejs", { periods });
-});
 
-// UserSchema
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  location: String,
-  gender: String,
-});
-app.use(express.urlencoded({ extended: true }));
+//-----------Education-------// Education------------------------- // Education -----------------------------//
+//-----------Education-------// Education ------------------------- // Education -----------------------------//
+//-----------Education-------// Education ------------------------- // Education -----------------------------//
 
-//Sign Up Error and Schema Layout
-app.post('/signup', async (req, res) => {
-  const { username, email, password, location, gender } = req.body;
-  const user = new User({ username, email, password, location, gender });
-  try {
-    await user.save();
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-const User = require('./models/user');
-
-
-// create a new user
-app.post('/users', async (req, res) => {
-  const { username, email, password, location, gender } = req.body;
-  const user = new User({ username, email, password, location, gender });
-  try {
-    await user.save();
-    res.redirect('/');
-    //In the case of an error, re-route
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-// Read all users
-app.get('/', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.render('index', { users });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-// read one user
-app.get('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await User.findById(id);
-    res.render('edit', { user });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-// update one user
-app.put('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const { username, email, password, location, gender } = req.body;
-  try {
-    await User.findByIdAndUpdate(id, { username, email, password, location, gender });
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-// delete one user
-app.delete('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    await User.findByIdAndDelete(id);
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
 
 // Education Route
 app.get('/education', (req, res) => {
-  res.render("education.ejs");
+ res.render("education.ejs");
 })
 
 
-// Listener
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-  });
+//-----------ABOUT-------// ABOUT------------------------- // ABOUT -----------------------------//
+//-----------ABOUT-------// ABOUT------------------------- // ABOUT -----------------------------//
+//-----------ABOUT-------// ABOUT ------------------------- // ABOUT-----------------------------//
 
+
+// About Route
+app.get('/views/about.ejs', (req, res) => {
+ res.render("about.ejs");
+})
+
+
+
+
+// SignUp
+app.get('/signup', (req, res) => {
+ res.render("signup.ejs");
+});
+
+
+// Login
+app.get('/login', (req, res) => {
+ res.render("login.ejs");
+});
+// Start server
+app.listen(3000, () => {
+ console.log('Server started on port 3000');
+});
+
+
+// ///
+
+// const MongoDBStore = require('connect-mongodb-session')(session);
+// const secretKey = process.env.SECRET_KEY;
+
+// //mongoDB
+// const store = new MongoDBStore({
+//   uri: 'mongodb+srv://menstrual:i0r3D6rayayhy5bo@sei.3zeldhq.mongodb.net/superperiodapp',
+//   collection: 'mySessions'
+// });
+
+// store.on('error', function(error) {
+//   console.log(error);
+// });
+
+// //// Set SESSION_SECRET environment variable
+// process.env.SESSION_SECRET = 'mysecretvalue';
+
+// //middleware
+// app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// //Session middleware
+// app.use(session({
+//   name: 'sessionId',
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   store: store,
+//   cookie: { secure: false }
+// }));
+
+// app.use(morgan('dev'));
+
+
+
+// //Mongoose
+// process.env.MONGODB_URI = 'mongodb+srv://menstrual:i0r3D6rayayhy5bo@sei.3zeldhq.mongodb.net/superperiodapp';
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Connected to MongoDB Atlas');
+// }).catch((error) => {
+//   console.log(error);
+// });
